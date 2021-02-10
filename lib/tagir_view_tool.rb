ff4f7890668099d00d1bc38f2f39a0dff0550714
tagirahmad <tagirbpl@gmail.com>
2021-02-10 11:57:26 +0300

    ViewTool initial commit

---
+++ lib/tagir_view_tool.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "tagir_view_tool/version"
+require 'tagir_view_tool/renderer'
+
+module TagirViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tagir_view_tool/renderer.rb
@@ -0,0 +1,9 @@
+module TagirViewTool
+  class Error < StandardError; end
+
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b>#{name} #{msg}".html_safe
+    end
+  end
+end
